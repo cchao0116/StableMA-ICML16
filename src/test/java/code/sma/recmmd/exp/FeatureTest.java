bc93195b7c1f6ed64a33a38b478139ceb3ac2de2
chench <chench@chench-PC>
2017-04-22 12:19:00 +0800

    daily update

--- src/test/java/code/sma/recmmd/exp/FeatureTest.java
+++ src/test/java/code/sma/recmmd/exp/FeatureTest.java
@@ -29,210 +29,215 @@ import code.sma.util.SerializeUtil;
 import code.sma.util.StringUtil;
 
 public class FeatureTest {
-	/** the logger instance */
-	protected final static Logger logger = Logger.getLogger(LoggerDefineConstant.SERVICE_TEST);
-	protected static String TEMP_FI = "C:/TEMP";
-	protected static String PYTHON_PATH = "C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python";
+    /** the logger instance */
+    protected final static Logger logger               = Logger
+        .getLogger(LoggerDefineConstant.SERVICE_TEST);
+    protected final static int    ITER_SEQ             = 1;
+    protected final static String OLD_MULTSK_R_PATTERN = "C:/Dataset/ml-10m/4/SerialObj/NIPS_Iter%d/GLOMA[%d].OBJ";
+    protected final static String OLD_EMBD_R_PATTERN   = "C:/Dataset/ml-1m/1/SerialObj/[RegSVD]_[200]_[%d]_[1]_[20].OBJ";
+    protected final static String NEW_EMBD_W_PATTERN   = "C:/Dataset/ml-1m/1/SerialObj/[RegSVD]_[200]_[%d]_[1]_[20].OBJ";
 
-	@Test
-	public void test() {
+    protected final static String TEMP_FI              = "C:/TEMP";
+    protected final static String PYTHON_PATH          = "C:/Users/Administrator/AppData/Local/Programs/Python/Python36/python";
 
-		Configures conf = ConfigureUtil.read("src/main/resources/samples/MTREC.properties");
-		String[] rootDirs = conf.getProperty("ROOT_DIRs").split("\\,");
+    @Test
+    public void test() {
 
-		for (String rootDir : rootDirs) {
-			LoggerUtil.info(logger, "1. loading " + rootDir);
-			conf.setProperty("ROOT_DIR", rootDir);
-//			String trainFile = rootDir + "trainingset";
-			String testFile = rootDir + "testingset";
+        Configures conf = ConfigureUtil.read("src/main/resources/samples/MTREC.properties");
+        String[] rootDirs = conf.getProperty("ROOT_DIRs").split("\\,");
 
-			String algName = conf.getProperty("ALG_NAME");
-			LoggerUtil.info(logger, "2. running " + algName);
+        for (String rootDir : rootDirs) {
+            LoggerUtil.info(logger, "1. loading " + rootDir);
+            conf.setProperty("ROOT_DIR", rootDir);
+            String testFile = rootDir + "testingset";
 
-			if (StringUtil.equalsIgnoreCase(algName, "MTREC")) {
-				AbstractDpncyChecker checker = new ModelDpncyChecker();
-				checker.handler(conf);
+            String algName = conf.getProperty("ALG_NAME");
+            LoggerUtil.info(logger, "2. running " + algName);
 
-//				MatlabFasionSparseMatrix tnMatrix = MatrixFileUtil.reads(trainFile);
-				MatlabFasionSparseMatrix ttMatrix = MatrixFileUtil.reads(testFile);
-				RecConfigEnv rce = new RecConfigEnv(conf);
-				RegularizedSVD rcmmd = extctGlbl(rce);
-				SerializeUtil.writeObject(rcmmd, "C:/Dataset/ml-1m/1/SerialObj/[RegSVD]_[200]_[3]_[1]_[20].OBJ");
-//				rcmmd.buildModel(tnMatrix, ttMatrix);
-				LoggerUtil.info(logger,
-						String.format("%s\n%s", rcmmd.toString(), rcmmd.evaluate(ttMatrix).printOneLine()));
-				
-			}
-		}
+            if (StringUtil.equalsIgnoreCase(algName, "MTREC")) {
+                AbstractDpncyChecker checker = new ModelDpncyChecker();
+                checker.handler(conf);
 
-	}
+                MatlabFasionSparseMatrix ttMatrix = MatrixFileUtil.reads(testFile);
+                RecConfigEnv rce = new RecConfigEnv(conf);
+                RegularizedSVD rcmmd = extctGlbl(rce);
+                SerializeUtil.writeObject(rcmmd, String.format(NEW_EMBD_W_PATTERN, ITER_SEQ));
+                LoggerUtil.info(logger, String.format("%s\n%s", rcmmd.toString(),
+                    rcmmd.evaluate(ttMatrix).printOneLine()));
 
-	protected RegularizedSVD extctGlbl(RecConfigEnv rce) {
-		RegularizedSVD auxRec = (RegularizedSVD) SerializeUtil
-				.readObject("C:/Dataset/ml-1m/1/SerialObj/[RegSVD]_[200]_[2]_[1]_[20].OBJ");
-		List<GLOMA> modls = new ArrayList<GLOMA>();
-		for (int i = 0; i < 30; i++) {
-			GLOMA recmmd = (GLOMA) SerializeUtil.readObject("C:/Dataset/ml-10m/4/SerialObj/NIPS_Iter3/GLOMA[" + i + "].OBJ");
-			modls.add(recmmd);
+            }
+        }
 
-		}
+    }
 
-		int threadNum = ((Double) rce.get("THREAD_NUMBER_VALUE")).intValue();
-		int userCount = auxRec.userCount;
-		int itemCount = auxRec.itemCount;
-		int featCount = auxRec.featureCount;
-		LoggerUtil.info(logger, "UFeats ");
-		DenseMatrix userFeature = new DenseMatrix(userCount, featCount);
-		try {
-			MultThread.modls = modls;
-			MultThread.auxRec = auxRec;
-			MultThread.maxID = userCount;
-			MultThread.isU = true;
+    protected RegularizedSVD extctGlbl(RecConfigEnv rce) {
+        RegularizedSVD auxRec = (RegularizedSVD) SerializeUtil
+            .readObject(String.format(OLD_EMBD_R_PATTERN, ITER_SEQ - 1));
+        List<GLOMA> modls = new ArrayList<GLOMA>();
+        for (int i = 0; i < 30; i++) {
+            GLOMA recmmd = (GLOMA) SerializeUtil
+                .readObject(String.format(OLD_MULTSK_R_PATTERN, ITER_SEQ, i));
+            modls.add(recmmd);
 
-			ExecutorService exec = Executors.newCachedThreadPool();
-			for (int t = 0; t < threadNum; t++) {
-				exec.execute(new MultThread(userFeature, TEMP_FI + t));
-			}
-			exec.shutdown();
-			exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
-		} catch (InterruptedException e) {
-			ExceptionUtil.caught(e, "User Features");
-		}
+        }
 
-		LoggerUtil.info(logger, "VFeats ");
-		DenseMatrix itemFeature = new DenseMatrix(itemCount, featCount);
-		try {
-			MultThread.maxID = itemCount;
-			MultThread.isU = false;
+        int threadNum = ((Double) rce.get("THREAD_NUMBER_VALUE")).intValue();
+        int userCount = auxRec.userCount;
+        int itemCount = auxRec.itemCount;
+        int featCount = auxRec.featureCount;
+        LoggerUtil.info(logger, "UFeats ");
+        DenseMatrix userFeature = new DenseMatrix(userCount, featCount);
+        try {
+            MultThread.modls = modls;
+            MultThread.auxRec = auxRec;
+            MultThread.maxID = userCount;
+            MultThread.isU = true;
 
-			ExecutorService exec = Executors.newCachedThreadPool();
-			for (int t = 0; t < threadNum; t++) {
-				exec.execute(new MultThread(itemFeature, TEMP_FI + t));
-			}
-			exec.shutdown();
-			exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
-		} catch (InterruptedException e) {
-			ExceptionUtil.caught(e, "Item Features");
-		}
+            ExecutorService exec = Executors.newCachedThreadPool();
+            for (int t = 0; t < threadNum; t++) {
+                exec.execute(new MultThread(userFeature, TEMP_FI + t));
+            }
+            exec.shutdown();
+            exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            ExceptionUtil.caught(e, "User Features");
+        }
 
-		return new RegularizedSVD(rce, userFeature, itemFeature);
-	}
+        LoggerUtil.info(logger, "VFeats ");
+        DenseMatrix itemFeature = new DenseMatrix(itemCount, featCount);
+        try {
+            MultThread.maxID = itemCount;
+            MultThread.isU = false;
 
-	public static class MultThread extends Thread {
-		public static List<GLOMA> modls;
-		public static RegularizedSVD auxRec;
-		public static int maxID;
-		public static boolean isU;
-		private static final Object MUTEX = new Object();
+            ExecutorService exec = Executors.newCachedThreadPool();
+            for (int t = 0; t < threadNum; t++) {
+                exec.execute(new MultThread(itemFeature, TEMP_FI + t));
+            }
+            exec.shutdown();
+            exec.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            ExceptionUtil.caught(e, "Item Features");
+        }
 
-		private DenseMatrix feats;
-		private String lTEMP;
-		private static int step;
+        return new RegularizedSVD(rce, userFeature, itemFeature);
+    }
 
-		public MultThread(DenseMatrix feats, String lTEMP) {
-			this.feats = feats;
-			this.lTEMP = lTEMP;
-			step = maxID / 50;
-		}
+    public static class MultThread extends Thread {
+        public static List<GLOMA>    modls;
+        public static RegularizedSVD auxRec;
+        public static int            maxID;
+        public static boolean        isU;
+        private static final Object  MUTEX = new Object();
 
-		private static int map() {
-			synchronized (MUTEX) {
-				if (maxID < 1) {
-					return -1;
-				} else {
-					maxID--;
-					if (maxID % step == 0) {
-						LoggerUtil.info(logger, "..." + ((50 * step - maxID) / step) * 100 + "p");
-					}
-					return maxID;
-				}
-			}
-		}
+        private DenseMatrix          feats;
+        private String               lTEMP;
+        private static int           step;
 
-		/**
-		 * @see java.lang.Thread#run()
-		 */
-		@Override
-		public void run() {
-			int ID = 0;
-			while ((ID = map()) != -1) {
-				if (isU) {
-					int u = ID;
-					String uFeats = getUFeats(modls, auxRec, u);
-					if (StringUtil.isBlank(uFeats)) {
-						continue;
-					}
+        public MultThread(DenseMatrix feats, String lTEMP) {
+            this.feats = feats;
+            this.lTEMP = lTEMP;
+            step = maxID / 50;
+        }
 
-					FileUtil.write(lTEMP, uFeats);
+        private static int map() {
+            synchronized (MUTEX) {
+                if (maxID < 1) {
+                    return -1;
+                } else {
+                    maxID--;
+                    if (maxID % step == 0) {
+                        LoggerUtil.info(logger, "..." + ((50 * step - maxID) / step) * 2 + "p");
+                    }
+                    return maxID;
+                }
+            }
+        }
 
-					DenseVector glbFeat = getGlblFeat(lTEMP);
-					feats.setRowRef(u, glbFeat);
-				} else {
-					int i = ID;
-					String vFeats = getVFeats(modls, auxRec, i);
-					if (StringUtil.isBlank(vFeats)) {
-						continue;
-					}
+        /**
+         * @see java.lang.Thread#run()
+         */
+        @Override
+        public void run() {
+            int ID = 0;
+            while ((ID = map()) != -1) {
+                if (isU) {
+                    int u = ID;
+                    String uFeats = getUFeats(modls, auxRec, u);
+                    if (StringUtil.isBlank(uFeats)) {
+                        continue;
+                    }
 
-					FileUtil.write(lTEMP, vFeats);
+                    FileUtil.write(lTEMP, uFeats);
 
-					DenseVector glbFeat = getGlblFeat(lTEMP);
-					feats.setRowRef(i, glbFeat);
-				}
-			}
+                    DenseVector glbFeat = getGlblFeat(lTEMP);
+                    feats.setRowRef(u, glbFeat);
+                } else {
+                    int i = ID;
+                    String vFeats = getVFeats(modls, auxRec, i);
+                    if (StringUtil.isBlank(vFeats)) {
+                        continue;
+                    }
 
-			if (FileUtil.exists(lTEMP)) {
-				FileUtil.delete(lTEMP);
-			}
-		}
+                    FileUtil.write(lTEMP, vFeats);
 
-		protected String getUFeats(List<GLOMA> modls, RegularizedSVD auxRec, int u) {
-			DenseVector avec = auxRec.userDenseFeatures.getRowRef(u);
+                    DenseVector glbFeat = getGlblFeat(lTEMP);
+                    feats.setRowRef(i, glbFeat);
+                }
+            }
 
-			StringBuilder uFeats = new StringBuilder();
-			for (GLOMA recmmd : modls) {
-				DenseVector vec = recmmd.userDenseFeatures.getRowRef(u);
-				if (vec != null) {
-					DenseVector feat = new DenseVector(recmmd.featureCount);
-					for (int k = 0; k < recmmd.featureCount; k++) {
-						feat.setValue(k, (vec.getValue(k) + avec.getValue(k)) / 2.0);
-					}
+            if (FileUtil.exists(lTEMP)) {
+                FileUtil.delete(lTEMP);
+            }
+        }
 
-					uFeats.append(feat.toString() + '\n');
-				}
-			}
-			return uFeats.toString();
-		}
+        protected String getUFeats(List<GLOMA> modls, RegularizedSVD auxRec, int u) {
+            DenseVector avec = auxRec.userDenseFeatures.getRowRef(u);
 
-		protected String getVFeats(List<GLOMA> modls, RegularizedSVD auxRec, int i) {
-			DenseVector avec = auxRec.itemDenseFeatures.getRowRef(i);
+            StringBuilder uFeats = new StringBuilder();
+            for (GLOMA recmmd : modls) {
+                DenseVector vec = recmmd.userDenseFeatures.getRowRef(u);
+                if (vec != null) {
+                    DenseVector feat = new DenseVector(recmmd.featureCount);
+                    for (int k = 0; k < recmmd.featureCount; k++) {
+                        feat.setValue(k, (vec.getValue(k) + avec.getValue(k)) / 2.0);
+                    }
 
-			StringBuilder uFeats = new StringBuilder();
-			for (GLOMA recmmd : modls) {
-				DenseVector vec = recmmd.itemDenseFeatures.getRowRef(i);
-				if (vec != null) {
-					DenseVector feat = new DenseVector(recmmd.featureCount);
-					for (int k = 0; k < recmmd.featureCount; k++) {
-						feat.setValue(k, (vec.getValue(k) + avec.getValue(k)) / 2.0);
-					}
+                    uFeats.append(feat.toString() + '\n');
+                }
+            }
+            return uFeats.toString();
+        }
 
-					uFeats.append(feat.toString() + '\n');
-				}
-			}
-			return uFeats.toString();
-		}
+        protected String getVFeats(List<GLOMA> modls, RegularizedSVD auxRec, int i) {
+            DenseVector avec = auxRec.itemDenseFeatures.getRowRef(i);
 
-		protected DenseVector getGlblFeat(String fi) {
-			String featrs = PythonExecUtil.exec(PYTHON_PATH, "src/main/resources/scripts/featr_extr.py", fi);
-			String[] fs = featrs.substring(featrs.indexOf("[") + 1, featrs.indexOf("]")).toString().trim()
-					.split("\\s+");
-			DenseVector fVec = new DenseVector(fs.length);
-			for (int i = 0; i < fs.length; i++) {
-				fVec.setValue(i, Double.valueOf(fs[i].trim()));
-			}
-			return fVec;
-		}
+            StringBuilder uFeats = new StringBuilder();
+            for (GLOMA recmmd : modls) {
+                DenseVector vec = recmmd.itemDenseFeatures.getRowRef(i);
+                if (vec != null) {
+                    DenseVector feat = new DenseVector(recmmd.featureCount);
+                    for (int k = 0; k < recmmd.featureCount; k++) {
+                        feat.setValue(k, (vec.getValue(k) + avec.getValue(k)) / 2.0);
+                    }
 
-	}
+                    uFeats.append(feat.toString() + '\n');
+                }
+            }
+            return uFeats.toString();
+        }
+
+        protected DenseVector getGlblFeat(String fi) {
+            String featrs = PythonExecUtil.exec(PYTHON_PATH,
+                "src/main/resources/scripts/featr_extr.py", fi);
+            String[] fs = featrs.substring(featrs.indexOf("[") + 1, featrs.indexOf("]")).toString()
+                .trim().split("\\s+");
+            DenseVector fVec = new DenseVector(fs.length);
+            for (int i = 0; i < fs.length; i++) {
+                fVec.setValue(i, Double.valueOf(fs[i].trim()));
+            }
+            return fVec;
+        }
+
+    }
 
 }
